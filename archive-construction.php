57ca0bbda38d6325e37d5f95a74721a86fe8c3ce
marudan555 <68321780+marudan555@users.noreply.github.com>
2020-07-31 16:57:19 +0900

    Update archive-construction.php

--- archive-construction.php
+++ archive-construction.php
@@ -11,38 +11,6 @@
 
 get_header(); ?>
 
-<?php
-    $get_cat = $_GET["cate"];
-    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
-    $args = array(
-        'post_type' => array('construction'),
-        'tax_query' => array('relation' => 'AND'),
-        // 'tax_query' => array(),
-        'posts_per_page' => -1,
-        'paged' => $paged,
-        'post_status' => 'publish',
-    );
-    $returnTaxonomy = create_taxonomy_array($get_cat,'cat_construction');
-    if($returnTaxonomy){
-        array_push($args['tax_query'],$returnTaxonomy);
-    }
-    function create_taxonomy_array($getCategory,$taxonomySlug){
-        if($getCategory){
-            if($taxonomySlug == "cat_construction"){
-                $taxonomy[] = array(
-                    'taxonomy' => $taxonomySlug,
-                    'field' => 'slug',
-                    'terms' => $getCategory,
-                );
-            }
-            return $taxonomy;
-        }else{
-            return;
-        }
-    }
-?>
-<?php $myquery = new WP_Query($args); ?>
-
 <main class="maincontents">
     <div class="wrap-content">
         <h2>施工例一覧<span class="line"></span></h2>
@@ -50,10 +18,10 @@ get_header(); ?>
             <div class="menu">
                 <div class="inner">
                     <ul class="cf">
-                        <li <?php if($get_cat == ""): ?>class="active"<?php endif; ?>><a href="<?php echo home_url("/"); ?>construction">すべて</a></li>
-                        <li class="line2 <?php if($get_cat == "新築・リフォームリノベーション"): ?>active<?php endif; ?>"><a href="<?php echo home_url("/"); ?>construction?cate=新築・リフォームリノベーション">新築・リフォーム<br>リノベーション</a></li>
-                        <li <?php if($get_cat == "お庭づくり"): ?>class="active"<?php endif; ?>><a href="<?php echo home_url("/"); ?>construction?cate=お庭づくり">お庭づくり</a></li>
-                        <li <?php if($get_cat == "施設・緑地"): ?>class="active"<?php endif; ?>><a href="<?php echo home_url("/"); ?>construction?cate=施設・緑地">施設・緑地</a></li>
+                        <li class="active"><a href="<?php echo get_archives_link('cat_construction'); ?>">すべて</a></li>
+                        <li class="line2"><a href="<?php echo get_term_link('new', 'cat_construction') ?>">新築・リフォーム<br>リノベーション</a></li>
+                        <li><a href="<?php echo get_term_link('garden', 'cat_construction') ?>">お庭づくり</a></li>
+                        <li><a href="<?php echo get_term_link('facility', 'cat_construction') ?>">施設・緑地</a></li>
                     </ul>
                 </div>
             </div>
@@ -61,18 +29,18 @@ get_header(); ?>
             <div class="cont_construction">
                 <div class="cont_inner">
                     <ul class="list_news">
-                        <?php if ($myquery->have_posts()):?>
-                            <?php while($myquery->have_posts()): $myquery->the_post(); ?>
+                        <?php if (have_posts()):?>
+                            <?php while(have_posts()): the_post(); ?>
                                 <?php
-                                    $tarms = get_the_terms($value->ID,'cat_construction');
+                                    $tarms = get_the_terms($post->ID,'cat_construction');
                                     $cate = $tarms[0]->name;
                                 ?>
                                 <li>
                                     <a href="<?php echo get_permalink($value->ID); ?>">
 										<p class="categoryname"><?php echo $cate; ?></p>
                                         <div class="wrap-img">
-                                        <?php if(get_field('thumbnail',$value->ID)): ?>
-                                            <img src="<?php echo get_field('thumbnail',$value->ID); ?>" alt="">
+                                        <?php if(get_field('thumbnail')): ?>
+                                            <img src="<?php echo get_field('thumbnail'); ?>" alt="">
                                         <?php else: ?>
                                             <img src="<?php echo get_template_directory_uri(); ?>/common/images/article_logo.jpg" alt="ロゴ">
                                         <?php endif; ?>
